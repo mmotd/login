c42d7f99b5e7fc6a7eacb73cdaa06b2140425331
Michael Albonesi <mikealbo@gmail.com>
2016-03-29 20:49:29 +0000

    fixed stuff

--- login.js
+++ login.js
@@ -1,38 +1,114 @@
 var Firebase = require('firebase');
 
-var ref = new Firebase('http://mmotd-login.firebaseio.com');
-
-//Grab form values
-//createEmailUser();
-//If click Google button
-//run google register method ref.onAuthWithPopup('Google', function(autData), if(!error){registerUser()}))
-
-function registerUser(ref, authData){
-	//Assume ref is the root database
-	//Check to see if the user already exists
-	ref.child('users').once('value', function(snap){
-          if(!snap.child(authData.uid).exists()){
-              ref.child('users').child(authData.uid).set({
-                  provider: authData.provider || '',
-                  name: authData.google.displayName
-              });
-          };
-          //Otherwise the user exists
-      });
-}
-
-function createEmailUser(ref, email, password) {
-  ref.createUser({
-    email: email,
-    password: password
-  }, function(error, userData) {
+  var ref;
+  
+  //console.log(ref)
+  
+  function init(url, handler){
+    ref = new Firebase(url);
+    
+    var authHandler = handler || function(authData) {
+      if (authData) {
+        console.log("Authenticated with uid:", authData.uid);
+        
+        //Will want to pull the username from the "users" table at this point, given the uid
+        var user = '';
+        ref.child('users/' + authData.uid).once('value', function(snapshot){
+          user = snapshot.val();
+        });
+        console.log(user.username || 'nothing');
+        
+      } else {
+        console.log("Client unauthenticated.")
+      }
+    }
+    
+    //Event listener for changes in authentication
+    //authHandler should be passed into this library for custom handling
+    ref.onAuth(authHandler);
+  }
+  
+  /**
+   * Create new user if user doesn't exist 
+   */
+  function saveUser(authData, username){
+    
+    /*
+    if(authData.provider){ 
+      provider = authData.provider;
+      username = authData.provider.displayName;
+    }
+    else { 
+      //provider = '';
+      //username = authData.password.email;
+    };
+    */
+    
+  	//Get user table
+  	ref.child('users').once('value', function(snapshot){
+      //If user doesn't exist
+      if(!snapshot.child(authData.uid).exists()){
+        //Create new user
+        ref.child('users').child(authData.uid).set({
+          username: username
+        });
+      };
+    });
+  }
+  
+  function handleRegister(error, authData){
     if (error) {
-      console.log("Error creating user:", error);
-    }
-    else {
-	//If no error, we still need to put the created user into our "users" table
-      	console.log("Successfully created user account with uid:", userData.uid);
-	registerUser(ref, userData);
-    }
-  });
-}
+        console.log("Error creating user:", error);
+        //Flash error
+      }
+      else {
+        console.log("Successfully created user account with uid:", authData.uid);
+      }
+  }
+  
+  function handleLogin(error, authData){
+    if (error) {
+        console.log("Error logging in user:", error);
+        //Flash an error
+      }
+      else {
+        console.log("Successfully logged in user:", authData);
+        //Let the auth monitor handle whatever happens...
+      }
+  }
+  
+  function registerOauthUser(provider){
+    ref.authWithOAuthPopup(provider, handleRegister)
+  }
+  
+  function registerEmailUser(email, password){
+    //This does not authenticate the user... ref.authWithPassword() must be called after.
+    ref.createUser({ email: email, password: password }, function(error, authData){
+      if (!error){
+        saveUser(authData, email.replace(/@.*/, ''));
+        emailLogin(email, password)
+      }
+      else{
+        console.log('yay')
+      }
+    });
+    //If no error, we still need to put the created user into our "users" table
+    
+  }
+  
+  function oauthLogin(provider){ 
+    ref.authWithOAuthPopup(provider, handleLogin);
+  }
+  
+  function emailLogin(email, password){
+    ref.authWithPassword({ email: email, password: password }, handleLogin);
+  }
+  
+  function logout(){
+    ref.unauth();
+  }
+
+exports.init = init;
+exports.registerEmailUser = registerEmailUser;
+exports.emailLogin = emailLogin;
+exports.logout = logout;
